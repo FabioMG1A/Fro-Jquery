916083f919eef2c531b20372054a6c8d76072266
FabioMG1A <34440826+FabioMG1A@users.noreply.github.com>
2018-11-16 12:39:47 +0100

    Add files via upload

---
+++ heldendinges/helden.js
@@ -0,0 +1,46 @@
+$(document).ready( function(){
+  // $('main').hide(3000).delay(1000).show(500)
+
+  // Muis events
+
+  $('figure img').mouseenter( function(){
+    $(this).fadeTo(600, 0.25);
+  }).mouseleave( function(){
+    $(this).fadeTo(1000, 1)
+  }).click( function(){
+    $(this).next().toggle(700);
+  })
+
+  Footer
+
+  $('footer').click( function(){
+    $(this).animate( {
+      top: '-=50px',
+      opacity: '-=.2'
+    }, 900)
+  } ).dblclick( function(){
+    $(this).animate( {
+      top: '0',
+      opacity: '1'
+    }, 900)
+  });
+});
+
+// navBar
+
+$("nav div").click(function() {
+  $("ul").slideToggle();
+  $("ul ul").css("display", "none");
+});
+
+$("ul li").click(function() {
+  $("ul ul").slideUp();
+  $(this).find('ul').slideToggle();
+});
+
+$(window).resize(function() {
+  if($(window).width() > 768) {
+        $("ul").removeAttr('style');
+  }
+});
+
